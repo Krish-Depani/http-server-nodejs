3e518ba5c84dbf26bfd10804e5de29b6515472a5
Krish-Depani <krishdepani@gmail.com>
2024-10-10 19:01:41 +0530

    extract url path

--- app/main.js
+++ app/main.js
@@ -1,7 +1,18 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const request = data.toString();
+    console.log(request);
+    const requestLine = request.split("\r\n")[0];
+    const urlPath = requestLine.split(" ")[1];
+
+    if (urlPath === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
   socket.on("close", () => {
     socket.end();
     socket.close();
