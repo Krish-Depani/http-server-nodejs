1dde18e52176641a6ef34b6782fe36c687d2b439
Krish-Depani <krishdepani@gmail.com>
2024-10-10 20:02:16 +0530

    files

--- app/main.js
+++ app/main.js
@@ -1,9 +1,18 @@
 const net = require("net");
+const fs = require("fs").promises;
+const path = require("path");
+
+let filesDirectory = null;
+
+const directoryFlagIndex = process.argv.indexOf("--directory");
+if (directoryFlagIndex !== -1 && process.argv.length > directoryFlagIndex + 1) {
+  filesDirectory = process.argv[directoryFlagIndex + 1];
+}
 
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
+  socket.on("data", async (data) => {
     const request = data.toString();
-    console.log(request);
+
     const lines = request.split("\r\n");
     const requestLine = lines[0];
     const headers = lines.slice(1);
@@ -34,6 +43,31 @@ const server = net.createServer((socket) => {
       socket.write(`Content-Length: ${contentLength}\r\n`);
       socket.write("\r\n");
       socket.write(agent);
+    } else if (urlPath.startsWith("/files/")) {
+      try {
+        const filename = urlPath.substring(7);
+        const filePath = path.join(filesDirectory, filename);
+
+        const stats = await fs.stat(filePath);
+
+        if (!stats.isFile()) {
+          throw new Error("The requested file is not a file.");
+        }
+
+        const fileContent = await fs.readFile(filePath);
+        const contentType = "application/octet-stream";
+        const contentLength = Buffer.byteLength(fileContent, "utf8");
+
+        socket.write("HTTP/1.1 200 OK\r\n");
+        socket.write(`Content-Type: ${contentType}\r\n`);
+        socket.write(`Content-Length: ${contentLength}\r\n`);
+        socket.write("\r\n");
+        socket.write(fileContent);
+      } catch (error) {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      } finally {
+        socket.end();
+      }
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
