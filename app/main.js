402c4ceaae3f0abdfc18b032e188dba3e4cf0101
Krish-Depani <krishdepani@gmail.com>
2024-10-10 19:32:10 +0530

    user agent

--- app/main.js
+++ app/main.js
@@ -3,8 +3,10 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-
-    const requestLine = request.split("\r\n")[0];
+    console.log(request);
+    const lines = request.split("\r\n");
+    const requestLine = lines[0];
+    const headers = lines.slice(1);
     const urlPath = requestLine.split(" ")[1];
 
     if (urlPath === "/") {
@@ -19,13 +21,25 @@ const server = net.createServer((socket) => {
       socket.write(`Content-Length: ${contentLength}\r\n`);
       socket.write("\r\n");
       socket.write(message);
+    } else if (urlPath === "/user-agent") {
+      let userAgent = headers.find((line) =>
+        line.toLowerCase().startsWith("user-agent:")
+      );
+      const agent = userAgent.split(":")[1].trim();
+      const contentType = "text/plain";
+      const contentLength = Buffer.byteLength(agent, "utf8");
+
+      socket.write("HTTP/1.1 200 OK\r\n");
+      socket.write(`Content-Type: ${contentType}\r\n`);
+      socket.write(`Content-Length: ${contentLength}\r\n`);
+      socket.write("\r\n");
+      socket.write(agent);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   });
   socket.on("close", () => {
     socket.end();
-    socket.close();
   });
 });
 
